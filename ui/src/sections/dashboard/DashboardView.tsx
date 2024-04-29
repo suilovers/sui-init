9e96c5d54f0c0c922e88d800af8f456c65aeadc8
ardagungunes <ardababa.123@outlook.com>
2024-04-29 21:27:57 +0300

    chore: add buttons in DashboardView

--- ui/src/sections/dashboard/DashboardView.tsx
+++ ui/src/sections/dashboard/DashboardView.tsx
@@ -1,31 +1,46 @@
-import { Grid } from '@mui/material';
-import Paper from '@mui/material/Paper';
-import { styled } from '@mui/material/styles';
-import './styles/DashboardView.css';
+import { Button, Grid, Stack } from "@mui/material";
+import Paper from "@mui/material/Paper";
+import { styled } from "@mui/material/styles";
+import "./styles/DashboardView.css";
 
 const Item = styled(Paper)(({ theme }) => ({
-    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-    ...theme.typography.body2,
-    padding: theme.spacing(1),
-    textAlign: 'center',
-    color: theme.palette.text.secondary
+  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: "center",
+  color: theme.palette.text.secondary,
 }));
 
 export default function DashboardView() {
-    return (
-        <Grid container spacing={2} direction="row" justifyContent="center" alignItems="center">
-            <Grid item xs={8}>
-                <Item>xs=8</Item>
-            </Grid>
-            <Grid item xs={4}>
-                <Item>xs=4</Item>
-            </Grid>
-            <Grid item xs={4}>
-                <Item>xs=4</Item>
-            </Grid>
-            <Grid item xs={8}>
-                <Item>xs=8</Item>
-            </Grid>
-        </Grid>
-    );
+  return (
+    <Grid
+      container
+      spacing={2}
+      direction="column"
+      justifyContent="center"
+      alignItems="center"
+      style={{ minHeight: "100vh" }} // This makes the Grid container take up the full height of the viewport
+    >
+      <Grid item xs={8}>
+        <Stack direction="column" spacing={2}>
+          <Button variant="contained">Pay</Button>
+        </Stack>
+      </Grid>
+      <Grid item xs={4}>
+        <Stack direction="column" spacing={2}>
+          <Button variant="contained">Sui Version</Button>
+        </Stack>
+      </Grid>
+      <Grid item xs={4}>
+        <Stack direction="column" spacing={2}>
+          <Button variant="contained">adress</Button>
+        </Stack>
+      </Grid>
+      <Grid item xs={8}>
+        <Stack direction="column" spacing={2}>
+          <Button variant="contained">envs</Button>
+        </Stack>
+      </Grid>
+    </Grid>
+  );
 }
