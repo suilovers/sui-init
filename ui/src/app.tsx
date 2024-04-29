0910b9ceb02e13e14616ef8f339739a8a9fba41d
ardagungunes <ardababa.123@outlook.com>
2024-04-29 20:14:22 +0300

    App.tsx

--- ui/src/app.tsx
+++ ui/src/app.tsx
@@ -1,18 +1,18 @@
-import CssBaseline from '@mui/material/CssBaseline';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-import { Router } from './routes';
+import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { Router } from "./routes";
 
 const darkTheme = createTheme({
-    palette: {
-        mode: 'dark'
-    }
+  palette: {
+    mode: "dark",
+  },
 });
 
 export default function App() {
-    return (
-        <ThemeProvider theme={darkTheme}>
-            <CssBaseline />
-            <Router />
-        </ThemeProvider>
-    );
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <Router />
+    </ThemeProvider>
+  );
 }
