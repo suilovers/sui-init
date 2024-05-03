466382cdadbfc438e8124070a431f8349108a3e1
abdullahbodur <abdullahbodur.abbdr@gmail.com>
2024-05-04 00:32:06 +0300

    chore: add new endpoints

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,4 @@
-import re
 from flask import Blueprint, Flask, request, jsonify
-import subprocess
-import json
 
 from pydantic import ValidationError
 from dto import (
@@ -17,6 +14,25 @@ from dto import (
     ClientExecuteSignedTxDTO,
     ClientDynamicFieldDTO,
     ClientPayAllSuiDTO,
+    ClientFaucetDTO,
+    ClientNewAddressDTO,
+    ClientMergeCoinDTO,
+    ClientGasDTO,
+    ClientObjectsDTO,
+    ClientObjectDTO,
+    ClientPublishDTO,
+    ClientSplitCoinDTO,
+    ClientSwitchDTO,
+    ClientProfileTransactionDTO,
+    ClientReplayTransactionDTO,
+    ClientReplayBatchDTO,
+    ClientReplayCheckpointDTO,
+    ClientTransferDTO,
+    ClientTransferSuiDTO,
+    ClientUpgradeDTO,
+    ClientVerifyBytecodeMeterDTO,
+    ClientTxBlockDTO,
+    ClientVerifySourceDTO,
 )
 from utils import sui_command, cat_command
 
@@ -132,6 +148,16 @@ def client_dynamic_field():
     return response
 
 
+@api_blueprint.route("/client/envs", methods=["GET"])
+def client_envs():
+    try:
+        command = ["client", "envs"]
+        response = sui_command(command)
+        return response
+    except ValidationError as e:
+        return {"error": str(e)}, 400
+
+
 @api_blueprint.route("/client/execute-signed-tx", methods=["POST"])
 def client_execute_signed_tx():
     try:
@@ -161,6 +187,79 @@ def client_execute_combined_signed_tx():
         return {"error": str(e)}, 400
 
 
+@api_blueprint.route("/client/faucet", methods=["GET"])
+def client_faucet():
+    try:
+        data = ClientFaucetDTO(**request.args)
+        command = ["client", "faucet"]
+        if data.address:
+            command.extend(["--address", data.address])
+        if data.url:
+            command.extend(["--url", data.url])
+        response = sui_command(command)
+        return response
+    except ValidationError as e:
+        return {"error": str(e)}, 400
+
+
+@api_blueprint.route("/client/gas", methods=["GET"])
+def client_gas():
+    try:
+        data = ClientGasDTO(**request.args)
+        command = ["client", "gas"]
+        if data.owner_address:
+            command.append(data.owner_address)
+        response = sui_command(command)
+        return response
+    except ValidationError as e:
+        return {"error": str(e)}, 400
+
+
+@api_blueprint.route("/client/merge-coin", methods=["POST"])
+def client_merge_coin():
+    try:
+        data = ClientMergeCoinDTO(**request.json)
+        command = [
+            "client",
+            "merge-coin",
+            "--primary-coin",
+            data.primary_coin,
+            "--coin-to-merge",
+            data.coin_to_merge,
+            "--gas-budget",
+            data.gas_budget,
+        ]
+        if data.gas:
+            command.extend(["--gas", data.gas])
+        if data.dry_run:
+            command.append("--dry-run")
+        if data.serialize_unsigned_transaction:
+            command.append("--serialize-unsigned-transaction")
+        if data.serialize_signed_transaction:
+            command.append("--serialize-signed-transaction")
+        response = sui_command(command)
+        return response
+    except ValidationError as e:
+        return {"error": str(e)}, 400
+
+
+@api_blueprint.route("/client/new-address", methods=["POST"])
+def client_new_address():
+    try:
+        data = ClientNewAddressDTO(**request.json)
+        command = ["client", "new-address", data.key_scheme]
+        if data.alias:
+            command.append(data.alias)
+        if data.word_length:
+            command.append(data.word_length)
+        if data.derivation_path:
+            command.append(data.derivation_path)
+        response = sui_command(command)
+        return response
+    except ValidationError as e:
+        return {"error": str(e)}, 400
+
+
 @api_blueprint.route("/client/new-env", methods=["POST"])
 def client_new_env():
     try:
@@ -168,6 +267,32 @@ def client_new_env():
         command = ["client", "new-env", "--alias", data.alias, "--rpc", data.rpc]
         if data.ws:
             command.extend(["--ws", data.ws])
+        response = sui_command(command)
+        return response
+    except ValidationError as e:
+        return {"error": str(e)}, 400
+
+
+@api_blueprint.route("/client/object", methods=["GET"])
+def client_object():
+    try:
+        data = ClientObjectDTO(**request.args)
+        command = ["client", "object", data.object_id]
+        if data.bcs:
+            command.append("--bcs")
+        response = sui_command(command)
+        return response
+    except ValidationError as e:
+        return {"error": str(e)}, 400
+
+
+@api_blueprint.route("/client/objects", methods=["GET"])
+def client_objects():
+    try:
+        data = ClientObjectsDTO(**request.args)
+        command = ["client", "objects"]
+        if data.owner_address:
+            command.append(data.owner_address)
         response = sui_command(command)
         return response
     except ValidationError as e:
@@ -238,6 +363,354 @@ def client_ptb():
         return response
     except ValidationError as e:
         return {"error": str(e)}, 400
+
+
+@api_blueprint.route("/client/publish", methods=["POST"])
+def client_publish():
+    data = ClientPublishDTO(**request.json)
+    command = ["client", "publish", "--gas-budget", data.gas_budget]
+    if data.package_path != ".":
+        command.append(data.package_path)
+    if data.dev:
+        command.append("--dev")
+    if data.test:
+        command.append("--test")
+    if data.doc:
+        command.append("--doc")
+    if data.install_dir:
+        command.extend(["--install-dir", data.install_dir])
+    if data.force:
+        command.append("--force")
+    if data.fetch_deps_only:
+        command.append("--fetch-deps-only")
+    if data.skip_fetch_latest_git_deps:
+        command.append("--skip-fetch-latest-git-deps")
+    if data.default_move_flavor:
+        command.extend(["--default-move-flavor", data.default_move_flavor])
+    if data.default_move_edition:
+        command.extend(["--default-move-edition", data.default_move_edition])
+    if data.dependencies_are_root:
+        command.append("--dependencies-are-root")
+    if data.silence_warnings:
+        command.append("--silence-warnings")
+    if data.warnings_are_errors:
+        command.append("--warnings-are-errors")
+    if data.no_lint:
+        command.append("--no-lint")
+    if data.lint:
+        command.append("--lint")
+    if data.gas:
+        command.extend(["--gas", data.gas])
+    if data.dry_run:
+        command.append("--dry-run")
+    if data.serialize_unsigned_transaction:
+        command.append("--serialize-unsigned-transaction")
+    if data.serialize_signed_transaction:
+        command.append("--serialize-signed-transaction")
+    if data.skip_dependency_verification:
+        command.append("--skip-dependency-verification")
+    if data.with_unpublished_dependencies:
+        command.append("--with-unpublished-dependencies")
+    response = sui_command(command)
+    return response
+
+
+@api_blueprint.route("/client/split-coin", methods=["POST"])
+def client_split_coin():
+    data = ClientSplitCoinDTO(**request.json)
+    command = [
+        "client",
+        "split-coin",
+        "--coin-id",
+        data.coin_id,
+        "--gas-budget",
+        data.gas_budget,
+    ]
+    if data.amounts:
+        command.extend(["--amounts"] + list(map(str, data.amounts)))
+    if data.count:
+        command.extend(["--count", str(data.count)])
+    if data.gas:
+        command.extend(["--gas", data.gas])
+    if data.dry_run:
+        command.append("--dry-run")
+    if data.serialize_unsigned_transaction:
+        command.append("--serialize-unsigned-transaction")
+    if data.serialize_signed_transaction:
+        command.append("--serialize-signed-transaction")
+    response = sui_command(command)
+    return response
+
+
+@api_blueprint.route("/client/switch", methods=["POST"])
+def client_switch():
+    data = ClientSwitchDTO(**request.json)
+    command = ["client", "switch"]
+    if data.address:
+        command.extend(["--address", data.address])
+    if data.env:
+        command.extend(["--env", data.env])
+    response = sui_command(command)
+    return response
+
+
+@api_blueprint.route("/client/tx-block", methods=["POST"])
+def client_tx_block():
+    data = ClientTxBlockDTO(**request.json)
+    command = ["client", "tx-block", data.digest]
+    response = sui_command(command)
+    return response
+
+
+@api_blueprint.route("/client/transfer", methods=["POST"])
+def client_transfer():
+    data = ClientTransferDTO(**request.json)
+    command = [
+        "client",
+        "transfer",
+        "--to",
+        data.to,
+        "--object-id",
+        data.object_id,
+        "--gas-budget",
+        data.gas_budget,
+    ]
+    if data.gas:
+        command.extend(["--gas", data.gas])
+    if data.dry_run:
+        command.append("--dry-run")
+    if data.serialize_unsigned_transaction:
+        command.append("--serialize-unsigned-transaction")
+    if data.serialize_signed_transaction:
+        command.append("--serialize-signed-transaction")
+    response = sui_command(command)
+    return response
+
+
+@api_blueprint.route("/client/transfer-sui", methods=["POST"])
+def client_transfer_sui():
+    data = ClientTransferSuiDTO(**request.json)
+    command = [
+        "client",
+        "transfer-sui",
+        "--to",
+        data.to,
+        "--sui-coin-object-id",
+        data.sui_coin_object_id,
+        "--gas-budget",
+        data.gas_budget,
+    ]
+    if data.amount:
+        command.extend(["--amount", data.amount])
+    if data.dry_run:
+        command.append("--dry-run")
+    if data.serialize_unsigned_transaction:
+        command.append("--serialize-unsigned-transaction")
+    if data.serialize_signed_transaction:
+        command.append("--serialize-signed-transaction")
+    response = sui_command(command)
+    return response
+
+
+@api_blueprint.route("/client/upgrade", methods=["POST"])
+def client_upgrade():
+    data = ClientUpgradeDTO(**request.json)
+    command = [
+        "client",
+        "upgrade",
+        "--upgrade-capability",
+        data.upgrade_capability,
+        "--gas-budget",
+        data.gas_budget,
+    ]
+    if data.package_path:
+        command.append(data.package_path)
+    if data.dev:
+        command.append("--dev")
+    if data.test:
+        command.append("--test")
+    if data.doc:
+        command.append("--doc")
+    if data.install_dir:
+        command.extend(["--install-dir", data.install_dir])
+    if data.force:
+        command.append("--force")
+    if data.fetch_deps_only:
+        command.append("--fetch-deps-only")
+    if data.skip_fetch_latest_git_deps:
+        command.append("--skip-fetch-latest-git-deps")
+    if data.default_move_flavor:
+        command.extend(["--default-move-flavor", data.default_move_flavor])
+    if data.default_move_edition:
+        command.extend(["--default-move-edition", data.default_move_edition])
+    if data.dependencies_are_root:
+        command.append("--dependencies-are-root")
+    if data.silence_warnings:
+        command.append("--silence-warnings")
+    if data.warnings_are_errors:
+        command.append("--warnings-are-errors")
+    if data.no_lint:
+        command.append("--no-lint")
+    if data.lint:
+        command.append("--lint")
+    if data.gas:
+        command.extend(["--gas", data.gas])
+    if data.dry_run:
+        command.append("--dry-run")
+    if data.serialize_unsigned_transaction:
+        command.append("--serialize-unsigned-transaction")
+    if data.serialize_signed_transaction:
+        command.append("--serialize-signed-transaction")
+    if data.skip_dependency_verification:
+        command.append("--skip-dependency-verification")
+    if data.with_unpublished_dependencies:
+        command.append("--with-unpublished-dependencies")
+    response = sui_command(command)
+    return response
+
+
+@api_blueprint.route("/client/verify-bytecode-meter", methods=["POST"])
+def client_verify_bytecode_meter():
+    data = ClientVerifyBytecodeMeterDTO(**request.json)
+    command = ["client", "verify-bytecode-meter", "--package", data.package]
+    if data.protocol_version:
+        command.extend(["--protocol-version", data.protocol_version])
+    if data.module:
+        for module in data.module:
+            command.extend(["--module", module])
+    if data.dev:
+        command.append("--dev")
+    if data.test:
+        command.append("--test")
+    if data.doc:
+        command.append("--doc")
+    if data.install_dir:
+        command.extend(["--install-dir", data.install_dir])
+    if data.force:
+        command.append("--force")
+    if data.fetch_deps_only:
+        command.append("--fetch-deps-only")
+    if data.skip_fetch_latest_git_deps:
+        command.append("--skip-fetch-latest-git-deps")
+    if data.default_move_flavor:
+        command.extend(["--default-move-flavor", data.default_move_flavor])
+    if data.default_move_edition:
+        command.extend(["--default-move-edition", data.default_move_edition])
+    if data.dependencies_are_root:
+        command.append("--dependencies-are-root")
+    if data.silence_warnings:
+        command.append("--silence-warnings")
+    if data.warnings_are_errors:
+        command.append("--warnings-are-errors")
+    if data.no_lint:
+        command.append("--no-lint")
+    if data.lint:
+        command.append("--lint")
+    response = sui_command(command)
+    return response
+
+
+@api_blueprint.route("/client/verify-source", methods=["POST"])
+def client_verify_source():
+    data = ClientVerifySourceDTO(**request.json)
+    command = ["client", "verify-source", data.package_path]
+    if data.dev:
+        command.append("--dev")
+    if data.test:
+        command.append("--test")
+    if data.doc:
+        command.append("--doc")
+    if data.install_dir:
+        command.extend(["--install-dir", data.install_dir])
+    if data.force:
+        command.append("--force")
+    if data.fetch_deps_only:
+        command.append("--fetch-deps-only")
+    if data.skip_fetch_latest_git_deps:
+        command.append("--skip-fetch-latest-git-deps")
+    if data.default_move_flavor:
+        command.extend(["--default-move-flavor", data.default_move_flavor])
+    if data.default_move_edition:
+        command.extend(["--default-move-edition", data.default_move_edition])
+    if data.dependencies_are_root:
+        command.append("--dependencies-are-root")
+    if data.silence_warnings:
+        command.append("--silence-warnings")
+    if data.warnings_are_errors:
+        command.append("--warnings-are-errors")
+    if data.no_lint:
+        command.append("--no-lint")
+    if data.lint:
+        command.append("--lint")
+    if data.verify_deps:
+        command.append("--verify-deps")
+    if data.skip_source:
+        command.append("--skip-source")
+    if data.address_override:
+        command.extend(["--address-override", data.address_override])
+    response = sui_command(command)
+    return response
+
+
+@api_blueprint.route("/client/profile-transaction", methods=["POST"])
+def client_profile_transaction():
+    data = ClientProfileTransactionDTO(**request.json)
+    command = ["client", "profile-transaction", "--tx-digest", data.tx_digest]
+    if data.profile_output:
+        command.extend(["--profile-output", data.profile_output])
+    if data.json:
+        command.append("--json")
+    response = sui_command(command)
+    return response
+
+
+@api_blueprint.route("/client/replay-transaction", methods=["POST"])
+def client_replay_transaction():
+    data = ClientReplayTransactionDTO(**request.json)
+    command = ["client", "replay-transaction", "--tx-digest", data.tx_digest]
+    if data.gas_info:
+        command.append("--gas-info")
+    if data.ptb_info:
+        command.append("--ptb-info")
+    if data.executor_version:
+        command.extend(["--executor-version", data.executor_version])
+    if data.protocol_version:
+        command.extend(["--protocol-version", data.protocol_version])
+    if data.json:
+        command.append("--json")
+    response = sui_command(command)
+    return response
+
+
+@api_blueprint.route("/client/replay-batch", methods=["POST"])
+def client_replay_batch():
+    data = ClientReplayBatchDTO(**request.json)
+    command = ["client", "replay-batch", "--path", data.path]
+    if data.terminate_early:
+        command.append("--terminate-early")
+    if data.json:
+        command.append("--json")
+    response = sui_command(command)
+    return response
+
+
+@api_blueprint.route("/client/replay-checkpoint", methods=["POST"])
+def client_replay_checkpoint():
+    data = ClientReplayCheckpointDTO(**request.json)
+    command = [
+        "client",
+        "replay-checkpoint",
+        "--start",
+        str(data.start),
+        "--end",
+        str(data.end),
+    ]
+    if data.terminate_early:
+        command.append("--terminate-early")
+    if data.json:
+        command.append("--json")
+    response = sui_command(command)
+    return response
 
 
 @api_blueprint.route("/client/private", methods=["GET"])
--- backend/dto.py
+++ backend/dto.py
@@ -1,19 +1,22 @@
 from pydantic import BaseModel
 from typing import List, Optional, Dict
 
+
 class StartDTO(BaseModel):
     network_config: str = None
     no_full_node: bool = False
+
 
 class ClientBalanceDTO(BaseModel):
     address: str
     coin_type: str = None
     with_coins: bool = False
-    
+
 
 class KeytoolConvertDTO(BaseModel):
     value: str
-    
+
+
 class ClientCallDTO(BaseModel):
     package: str
     module: str
@@ -25,24 +28,28 @@ class ClientCallDTO(BaseModel):
     dry_run: bool = False
     serialize_unsigned_transaction: bool = False
     serialize_signed_transaction: bool = False
-    
+
+
 class ClientDynamicFieldDTO(BaseModel):
     object_id: str
     cursor: Optional[str] = None
     limit: Optional[int] = 50
-    
+
 
 class ClientExecuteSignedTxDTO(BaseModel):
     tx_bytes: str
     signatures: List[str]
 
+
 class ClientExecuteCombinedSignedTxDTO(BaseModel):
     signed_tx_bytes: str
+
 
 class ClientNewEnvDTO(BaseModel):
     alias: str
     rpc: str
     ws: Optional[str]
+
 
 class ClientPayDTO(BaseModel):
     input_coins: List[str]
@@ -50,16 +57,19 @@ class ClientPayDTO(BaseModel):
     amounts: List[str]
     gas_budget: str
 
+
 class ClientPayAllSuiDTO(BaseModel):
     input_coins: List[str]
     recipient: str
     gas_budget: str
+
 
 class ClientPaySuiDTO(BaseModel):
     input_coins: List[str]
     recipients: List[str]
     amounts: List[str]
     gas_budget: str
+
 
 class ClientPtbDTO(BaseModel):
     assign: Optional[Dict[str, str]]
@@ -77,3 +87,215 @@ class ClientPtbDTO(BaseModel):
     serialize_signed_transaction: Optional[bool]
     summary: Optional[bool]
     warn_shadows: Optional[bool]
+
+
+class ClientFaucetDTO(BaseModel):
+    address: Optional[str] = None
+    url: Optional[str] = None
+
+
+class ClientGasDTO(BaseModel):
+    owner_address: Optional[str] = None
+
+
+class ClientMergeCoinDTO(BaseModel):
+    primary_coin: str
+    coin_to_merge: str
+    gas: Optional[str] = None
+    gas_budget: str
+    dry_run: bool = False
+    serialize_unsigned_transaction: bool = False
+    serialize_signed_transaction: bool = False
+
+
+class ClientNewAddressDTO(BaseModel):
+    key_scheme: str
+    alias: Optional[str] = None
+    word_length: Optional[str] = None
+    derivation_path: Optional[str] = None
+
+
+class ClientObjectDTO(BaseModel):
+    object_id: str
+    bcs: bool = False
+
+
+class ClientObjectsDTO(BaseModel):
+    owner_address: Optional[str] = None
+
+
+class ClientPublishDTO(BaseModel):
+    package_path: Optional[str] = "."
+    dev: Optional[bool] = False
+    test: Optional[bool] = False
+    doc: Optional[bool] = False
+    install_dir: Optional[str] = None
+    force: Optional[bool] = False
+    fetch_deps_only: Optional[bool] = False
+    skip_fetch_latest_git_deps: Optional[bool] = False
+    default_move_flavor: Optional[str] = None
+    default_move_edition: Optional[str] = None
+    dependencies_are_root: Optional[bool] = False
+    silence_warnings: Optional[bool] = False
+    warnings_are_errors: Optional[bool] = False
+    no_lint: Optional[bool] = False
+    lint: Optional[bool] = False
+    gas: Optional[str] = None
+    gas_budget: str
+    dry_run: Optional[bool] = False
+    serialize_unsigned_transaction: Optional[bool] = False
+    serialize_signed_transaction: Optional[bool] = False
+    skip_dependency_verification: Optional[bool] = False
+    with_unpublished_dependencies: Optional[bool] = False
+
+
+class ClientSplitCoinDTO(BaseModel):
+    coin_id: str
+    amounts: Optional[List[int]] = None
+    count: Optional[int] = None
+    gas: Optional[str] = None
+    gas_budget: str
+    dry_run: Optional[bool] = False
+    serialize_unsigned_transaction: Optional[bool] = False
+    serialize_signed_transaction: Optional[bool] = False
+
+
+class ClientSwitchDTO(BaseModel):
+    address: Optional[str] = None
+    env: Optional[str] = None
+
+
+class ClientTxBlockDTO(BaseModel):
+    digest: str
+
+
+class ClientTransferDTO(BaseModel):
+    to: str
+    object_id: str
+    gas: Optional[str] = None
+    gas_budget: str
+    dry_run: Optional[bool] = False
+    serialize_unsigned_transaction: Optional[bool] = False
+    serialize_signed_transaction: Optional[bool] = False
+
+
+class ClientTransferSuiDTO(BaseModel):
+    to: str
+    sui_coin_object_id: str
+    gas_budget: str
+    amount: Optional[str] = None
+    dry_run: Optional[bool] = False
+    serialize_unsigned_transaction: Optional[bool] = False
+    serialize_signed_transaction: Optional[bool] = False
+
+
+class ClientUpgradeDTO(BaseModel):
+    upgrade_capability: str
+    gas_budget: str
+    package_path: Optional[str] = "."
+    dev: Optional[bool] = False
+    test: Optional[bool] = False
+    doc: Optional[bool] = False
+    install_dir: Optional[str] = None
+    force: Optional[bool] = False
+    fetch_deps_only: Optional[bool] = False
+    skip_fetch_latest_git_deps: Optional[bool] = False
+    default_move_flavor: Optional[str] = None
+    default_move_edition: Optional[str] = None
+    dependencies_are_root: Optional[bool] = False
+    silence_warnings: Optional[bool] = False
+    warnings_are_errors: Optional[bool] = False
+    no_lint: Optional[bool] = False
+    lint: Optional[bool] = False
+    gas: Optional[str] = None
+    dry_run: Optional[bool] = False
+    serialize_unsigned_transaction: Optional[bool] = False
+    serialize_signed_transaction: Optional[bool] = False
+    skip_dependency_verification: Optional[bool] = False
+    with_unpublished_dependencies: Optional[bool] = False
+
+
+class ClientVerifyBytecodeMeterDTO(BaseModel):
+    package: Optional[str] = "."
+    protocol_version: Optional[str] = None
+    module: Optional[List[str]] = None
+    dev: Optional[bool] = False
+    test: Optional[bool] = False
+    doc: Optional[bool] = False
+    install_dir: Optional[str] = None
+    force: Optional[bool] = False
+    fetch_deps_only: Optional[bool] = False
+    skip_fetch_latest_git_deps: Optional[bool] = False
+    default_move_flavor: Optional[str] = None
+    default_move_edition: Optional[str] = None
+    dependencies_are_root: Optional[bool] = False
+    silence_warnings: Optional[bool] = False
+    warnings_are_errors: Optional[bool] = False
+    no_lint: Optional[bool] = False
+    lint: Optional[bool] = False
+
+
+class ClientVerifySourceDTO(BaseModel):
+    package_path: Optional[str] = "."
+    dev: Optional[bool] = False
+    test: Optional[bool] = False
+    doc: Optional[bool] = False
+    install_dir: Optional[str] = None
+    force: Optional[bool] = False
+    fetch_deps_only: Optional[bool] = False
+    skip_fetch_latest_git_deps: Optional[bool] = False
+    default_move_flavor: Optional[str] = None
+    default_move_edition: Optional[str] = None
+    dependencies_are_root: Optional[bool] = False
+    silence_warnings: Optional[bool] = False
+    warnings_are_errors: Optional[bool] = False
+    no_lint: Optional[bool] = False
+    lint: Optional[bool] = False
+    verify_deps: Optional[bool] = False
+    skip_source: Optional[bool] = False
+    address_override: Optional[str] = None
+
+
+class ClientVerifySourceDTO(BaseModel):
+    package_path: Optional[str] = "."
+    dev: Optional[bool] = False
+    test: Optional[bool] = False
+    doc: Optional[bool] = False
+    install_dir: Optional[str] = None
+    force: Optional[bool] = False
+    fetch_deps_only: Optional[bool] = False
+    skip_fetch_latest_git_deps: Optional[bool] = False
+    default_move_flavor: Optional[str] = None
+    default_move_edition: Optional[str] = None
+    dependencies_are_root: Optional[bool] = False
+    silence_warnings: Optional[bool] = False
+    warnings_are_errors: Optional[bool] = False
+    no_lint: Optional[bool] = False
+    lint: Optional[bool] = False
+    verify_deps: Optional[bool] = False
+    skip_source: Optional[bool] = False
+    address_override: Optional[str] = None
+
+
+class ClientProfileTransactionDTO(BaseModel):
+    tx_digest: str
+    profile_output: Optional[str] = None
+
+
+class ClientReplayTransactionDTO(BaseModel):
+    tx_digest: str
+    gas_info: Optional[bool] = False
+    ptb_info: Optional[bool] = False
+    executor_version: Optional[str] = None
+    protocol_version: Optional[str] = None
+
+
+class ClientReplayBatchDTO(BaseModel):
+    path: str
+    terminate_early: Optional[bool] = False
+
+
+class ClientReplayCheckpointDTO(BaseModel):
+    start: int
+    end: int
+    terminate_early: Optional[bool] = False
