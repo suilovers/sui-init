ef2d1c6e619f33a45a866d766956ced3f33f7f57
abdullahbodur <abdullahbodur.abbdr@gmail.com>
2024-05-04 22:29:07 +0300

    fix: solve

--- backend/utils.py
+++ backend/utils.py
@@ -2,24 +2,20 @@ import json
 import subprocess
 import os
 
+
 def sui_command(sui_commands, isJson=True, name=None):
     if isJson:
-        return json.loads(subprocess.check_output(['sui'] + sui_commands + ['--json']))
+        return json.loads(subprocess.check_output(["sui"] + sui_commands + ["--json"]))
     else:
-        result = subprocess.run(['sui'] + sui_commands, capture_output=True, text=True)
+        result = subprocess.run(["sui"] + sui_commands, capture_output=True, text=True)
         return {name: result.stdout.strip()}
-    
+
+
 def cat_command(file_path, isJson=True, name=None):
     expanded_path = os.path.expanduser(file_path)
-    with open(expanded_path, 'r') as f:
+    with open(expanded_path, "r") as f:
         file = json.load(f)
         if isJson:
             return json.loads(json.dumps(file))
         else:
             return {name: file}
-        
-def add_optional_field(commands, optional_fields):
-    for field in optional_fields:
-        if field not in data:
-            data[field] = None
-    return data
