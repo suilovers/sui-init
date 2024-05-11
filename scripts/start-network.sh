c8a2b70dbe2e30b1bde04924a4c8b949be018321
abdullahbodur <abdullahbodur.abbdr@gmail.com>
2024-05-11 22:47:15 +0300

    chore: change port 7000 to 9000

--- scripts/start-network.sh
+++ scripts/start-network.sh
@@ -4,7 +4,7 @@
 sui-test-validator \
     --config-dir /root/.sui/sui_config \
     --indexer-rpc-port 9124 \
-    --fullnode-rpc-port 7000 \
+    --fullnode-rpc-port 9000 \
     --graphql-host 127.0.0.1 \
     --graphql-port 8000 \
     --with-indexer \ 
