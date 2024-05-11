3fc49ab3cb7fb272c98f42d6a488f51e766fe281
abdullahbodur <abdullahbodur.abbdr@gmail.com>
2024-05-11 22:18:58 +0300

    chore: update

--- scripts/start-network.sh
+++ scripts/start-network.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 # start the network
-sui-test-validator --config-dir /root/.sui/sui_config --indexer-rpc-port 9124 --fullnode-rpc-port 7000
+sui-test-validator \
+    --config-dir /root/.sui/sui_config \
+    --indexer-rpc-port 9124 \
+    --fullnode-rpc-port 7000 \
+    --graphql-host 127.0.0.1 \
+    --graphql-port 8000 \
+    --with-indexer \ 
 
 tail -f /dev/null
