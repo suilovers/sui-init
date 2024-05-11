f85464252e3dd3eea1e192656ba41f99a7d8c00c
abdullahbodur <abdullahbodur.abbdr@gmail.com>
2024-05-11 22:15:48 +0300

    chore: update

--- scripts/start-network.sh
+++ scripts/start-network.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # start the network
-sui-test-validator --config-dir /root/.sui/sui_config
+sui-test-validator --config-dir /root/.sui/sui_config --indexer-rpc-port 9124 --fullnode-rpc-port 7000
 
 tail -f /dev/null
